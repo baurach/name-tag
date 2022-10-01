efb2d38f84f7dfd4432c6aa1ed045703b00a21ff
Rachael Bautista <BAUTISTARACHAEL@GMAIL.COM>
2022-10-01 16:37:25 -0700

    JS demo updates

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 // grab DOM elements
 
 // set event listeners
+
+
+const nameDisplay = document.getElementById('name-display');
+console.log(nameDisplay);
+
+const greeting = document.getElementById('greeting');
+
+nameDisplay.textContent = 'Fred';
+nameDisplay.classList.add('purple');
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+});
+
+const updateLanguage = document.getElementById('update-language');
+updateLanguage.addEventListener('click', () => {
+    console.log('language selected');
+    const language = document.getElementById('Language');
+    greeting.textContent = language.value;
+});
